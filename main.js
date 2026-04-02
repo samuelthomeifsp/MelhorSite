f7e1df44213854d59585594a94ce3344d07e29ef
Samuel Ribeiro Thomé <samuel.thome@aluno.ifsp.edu.br>
2026-04-02 12:31:18 -0300

    Commit inicial do projeto

---
+++ main.js
@@ -0,0 +1,19 @@
+const backgroundAudio = document.getElementById("background-audio")
+backgroundAudio.play()
+
+const chooseFile = document.getElementById("image-upload");
+const imgPreview = document.getElementById("image-preview");
+
+chooseFile.addEventListener("change", function () {
+    const files = this.files;
+    if (files && files.length) {
+        const selectedFile = files[0];
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+            imgPreview.src = reader.result;
+        }, false);
+
+        reader.readAsDataURL(selectedFile);
+    }
+});
